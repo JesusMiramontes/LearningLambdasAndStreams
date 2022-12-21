648d9f67f1600f6c01c9e45684d7c9c195988b6e
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-12-21 01:22:03 -0600

    Sort

--- src/com/miramontes/exercises/five/streams/Main.java
+++ src/com/miramontes/exercises/five/streams/Main.java
@@ -1,6 +1,7 @@
 package com.miramontes.exercises.five.streams;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,10 +10,15 @@ public class Main {
         List<Person> people = getPeople();
 
         // Filter
+        System.out.println("\n\n***Filter***");
         List<Person> females = people.stream().filter(p -> p.getGender().equals(Gender.FEMALE)).collect(Collectors.toList());
         females.forEach(System.out::println);
 
-
+        // Sort
+        System.out.println("\n\n***Sorted***");
+        // Reverse sorted first by age then by gender
+        List<Person> sorted = people.stream().sorted(Comparator.comparing(Person::getAge).reversed().thenComparing(Person::getGender)).collect(Collectors.toList());
+        sorted.forEach(System.out::println);
     }
 
     private static List<Person> getPeople() {
