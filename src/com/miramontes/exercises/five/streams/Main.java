54be95f6c50e5a0ae1c49a348900ae3c5fd84a5a
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-12-21 01:16:25 -0600

    Filter

--- src/com/miramontes/exercises/five/streams/Main.java
+++ src/com/miramontes/exercises/five/streams/Main.java
@@ -2,10 +2,17 @@ package com.miramontes.exercises.five.streams;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = getPeople();
+
+        // Filter
+        List<Person> females = people.stream().filter(p -> p.getGender().equals(Gender.FEMALE)).collect(Collectors.toList());
+        females.forEach(System.out::println);
+
+
     }
 
     private static List<Person> getPeople() {
