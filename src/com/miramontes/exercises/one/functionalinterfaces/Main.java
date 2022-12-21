d651714cd744ae2d6a6f884796a095b1e647db2c
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-12-20 21:47:27 -0600

    Functional method.

--- src/com/miramontes/exercises/one/functionalinterfaces/Main.java
+++ src/com/miramontes/exercises/one/functionalinterfaces/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Greeting greeting = new HelloGreeting();
         greeting.sayHello();
+
+        // Functional
+        Greeting greeting2 = new Greeting() {
+            @Override
+            public void sayHello() {
+                System.out.println("Hi!");
+            }
+        };
+
+        greeting2.sayHello();
     }
 }
