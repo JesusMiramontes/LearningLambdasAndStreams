b583acdbf4cd1e419991e2222407f76306ea26f0
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2022-12-20 22:04:32 -0600

    Same method but using existing functional interface.

--- src/com/miramontes/exercises/two/lambdas/Calculator.java
+++
@@ -1,6 +0,0 @@
-package com.miramontes.exercises.two.lambdas;
-
-@FunctionalInterface
-public interface Calculator {
-    int calculate(int a, int b);
-}
--- src/com/miramontes/exercises/two/lambdas/Main.java
+++ src/com/miramontes/exercises/two/lambdas/Main.java
@@ -1,17 +1,18 @@
 package com.miramontes.exercises.two.lambdas;
 
 import java.util.Random;
+import java.util.function.IntBinaryOperator;
 
 public class Main {
     public static void main(String[] args) {
 
         // Lambda expression with params and return.
-        Calculator calculator = (int a, int b) -> {
+        IntBinaryOperator calculator = (int a, int b) -> {
             Random random = new Random();
             int randNumb = random.nextInt(50);
             return a * b + randNumb;
         };
 
-        System.out.println("calculator.calculate(1,2) = " + calculator.calculate(1,2));
+        System.out.println("calculator.calculate(1,2) = " + calculator.applyAsInt(1,2));
     }
 }
